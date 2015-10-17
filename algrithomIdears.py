5985861461b49784fbef1223f1ce158426ea2062
Richard de Mellow <richdemellow@gmail.com>
2015-10-17 14:00:21 +0100

    adding project files and python test tile to git

---
+++ algrithomIdears.py
@@ -0,0 +1,30 @@
+
+ajmatrix = [[1,0,1,1],
+         [1,1,0,0],
+         [0,1,1,0],
+         [0,1,1,1],
+         [1,0,0,1],
+         [0,1,0,0]
+        ]
+
+def GenrateSkill(matrix):
+  kMatrix = matrix
+  people = [ 0 for i in range(len(matrix[0]))]
+
+  for i, row in enumerate(kMatrix):
+    currentEx = 0
+    for j, event in enumerate(row):
+      
+      if event:
+        people[j] += 1
+        currentEx += 1
+        kMatrix[i][j] = currentEx
+
+  
+
+  print people
+  return kMatrix
+
+
+print (GenrateSkill(ajmatrix))
+
